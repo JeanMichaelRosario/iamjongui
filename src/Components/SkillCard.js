d2f06d7d6b38d7599522ab36edd9d308b082ca64
JeanMichaelRosario <jmrosario94@gmail.com>
2019-02-21 15:11:23 +0000

    adding more info to skill cards

--- src/Components/SkillCard.js
+++ src/Components/SkillCard.js
@@ -4,7 +4,17 @@ import React from 'react';
 const SkillCard = ({ icon }) => {
     return(
         <div className="w-50">
-            <i className={icon}></i>
+            <div>
+                <i className={icon}></i>
+                <h1 className="f4">Mimi Whitehouse</h1>
+                <hr className="mw3 bb bw1 b--black-10" />
+            </div>
+            <p className="ph2">
+                Quite affectionate and outgoing. 
+                She loves to get chin scratches 
+                and will roll around on the floor 
+                waiting for you give her more of them.
+            </p>
         </div>
     );
 };

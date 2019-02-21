a94fbd11f4fa93bd6a47d261d2de7b14afd9cc37
JeanMichaelRosario <jmrosario94@gmail.com>
2019-02-21 16:02:10 +0000

    creating a folder for images and moving images to that folder

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -3,10 +3,26 @@ import ListOfSkills from './ListOfSkills';
 
 const AboutMe = () => {
     const skills = [
-        "fas fa-people-carry",
-        "fas fa-user-tie",
-        "fas fa-handshake",
-        "fas fa-laptop-code"
+        {
+            icon: 'fas fa-people-carry',
+            title: 'Colaborator',
+            text: 'I like to work with other people and having fun building apps together'
+        },
+        {
+            icon: 'fas fa-user-tie',
+            title: 'Confidence',
+            text: 'I have the experience to be sure about what I do, and to know when I made a mistake'
+        },
+        {
+            icon: 'fas fa-handshake',
+            title: 'Commitment',
+            text: 'I am always exited about what I am making, and being a good teammate'
+        },
+        {
+            icon: 'fas fa-laptop-code',
+            title: 'Happy Coder',
+            text: 'I love what I do. Every day is a new opportunity to grow, learn and have fun'
+        }
     ]
 
     return(

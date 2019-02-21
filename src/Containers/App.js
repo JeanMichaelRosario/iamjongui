fe2b9cb3ebab898ae4e75359b6ae6a1b7f9ae4b6
JeanMichaelRosario <jmrosario94@gmail.com>
2019-02-21 14:50:50 +0000

    creating footer and using it as contact section

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -4,21 +4,27 @@ import '../Styles/App.css';
 import Header from "../Components/Header";
 import About from './About';
 import Portfolio from './Portfolio';
+import Footer from "../Components/Footer";
 import 'tachyons';
 
 class App extends Component {
   render() {
     return (
-      <div className="mh3">
-        <Header />
-        <div className="tc">
-          <section id="About">
-            <About/>
-          </section>
-          <section id="Portfolio">
-            <Portfolio />
-          </section>
+      <div>
+        <div className="mh3">
+          <Header />
+          <div className="tc">
+            <section id="About">
+              <About/>
+            </section>
+            <section id="Portfolio">
+              <Portfolio />
+            </section>
+          </div>
         </div>
+        <section id="Contact">
+          <Footer />
+        </section>
       </div>
     );
   }

a41ca7028d556030988eb71163ad59a7410b3272
JeanMichaelRosario <jmrosario94@gmail.com>
2019-02-24 14:38:36 +0000

    removing my css and using tachyons

--- src/Components/ProjectsCard.js
+++ src/Components/ProjectsCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ProjectsCard = ({ image, url }) => {
     return(
         <a href={url} className="w-50 w-25-l link overflow-hidden pa2">
-            <div role="img" aria-label="Primer movie" className="grow aspect-ratio--1x1 h-25 project-image w-100" style={{ backgroundImage: `url(/Images/Projects/${image}.PNG)`}}></div>
+            <div role="img" aria-label="Primer movie" className="grow aspect-ratio--1x1 h-25 cover bg-left w-100" style={{ backgroundImage: `url(/Images/Projects/${image}.PNG)`}}></div>
         </a>
     );
 };

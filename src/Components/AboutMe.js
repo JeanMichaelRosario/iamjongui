93ecc50cce2a5ed99ed7c35beed5059b442247ab
JeanMichaelRosario <jmrosario94@gmail.com>
2019-02-24 02:47:03 +0000

    chaging personal skills text

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -5,23 +5,23 @@ const AboutMe = () => {
     const skills = [
         {
             icon: 'fas fa-people-carry',
-            title: 'Colaborator',
-            text: 'I like to work with other people and having fun building apps together'
+            title: 'Coworker',
+            text: 'I like to work with other people and having fun building apps together.'
         },
         {
             icon: 'fas fa-user-tie',
             title: 'Confidence',
-            text: 'I have the experience to be sure about what I do, and to know when I made a mistake'
+            text: 'I have 4+ years of experience in this area, I know what I have to do and how get it done.'
         },
         {
             icon: 'fas fa-handshake',
             title: 'Commitment',
-            text: 'I am always exited about what I am making, and being a good teammate'
+            text: 'My work is what I am passion about, that is why I take care to the details of everything I do'
         },
         {
             icon: 'fas fa-laptop-code',
             title: 'Happy Coder',
-            text: 'I love what I do. Every day is a new opportunity to grow, learn and have fun'
+            text: 'I love what I do. Every day is a new opportunity to grow, learn and have fun.'
         }
     ]
 

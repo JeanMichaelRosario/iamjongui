84da53b6c69e33b743909393b1e57f74926bea6f
JeanMichaelRosario <jmrosario94@gmail.com>
2019-02-25 20:05:28 +0000

    adding info about me

--- src/Components/Introduction.js
+++ src/Components/Introduction.js
@@ -5,16 +5,25 @@ const Introduction = () => {
         <div className="w-100">
             <img alt="me" src='../Images/me.png' className="dib w4 h4"/>
             <h3>Who am I ?</h3>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                sed do eiusmod tempor incididunt ut labore et dolore magna 
-                aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
-                ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                Duis aute irure dolor in reprehenderit in voluptate velit 
-                esse cillum dolore eu fugiat nulla pariatur. Excepteur 
-                sint occaecat cupidatat non proident, sunt in culpa qui 
-                officia deserunt mollit anim id est laborum.
-            </p>
+            <div className="tr">
+                <p>
+                    I am a Software Developer, I had work for companies like:&nbsp;
+                    <a href="#portfolio">nzxt</a>,&nbsp;<a href="#portfolio">financefactory</a> and&nbsp;
+                    <a href="#portfolio">mctekk</a>. You can see some of the tools I use on the left.
+                    As a self-learned developer, I keep learning and digging into the internet to 
+                    use new stuff in projects I am working in. I love to work with other people because
+                    that helps me to grow faster and I like to discuss different ideas with the team</p> 
+                <p>
+                    I'm extremely loyal and passionate to the works that I'm involved with,
+                    and I'm always looking for new challenges. Through the experience that
+                    I achieved from work, I was able to guide many people with my leadership
+                    skills.
+                </p>
+                <p> Please check my work below, and please
+                    don't hesitate to contact me. Also feel free to download my resume and
+                    check it out. 
+                </p>
+            </div>
         </div>
     );
 };
